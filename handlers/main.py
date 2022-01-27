23c22bf2e7acc7d89d88000f46d2f67a8883dfca
Jemacivan <63158240+Jemacivan@users.noreply.github.com>
2022-01-27 15:59:56 +0200

    Добавлена поддержка видео, в описание добавлен ник

--- handlers/main.py
+++ handlers/main.py
@@ -8,10 +8,14 @@ from load import dp, bot, config
 from dcbot import bot as dbot
 
 
-async def sender(photo):
+async def sender(photo: str, username: str | None):
+    if username:
+        message = f'Отправлено от: {username}'
+    else:
+        message = 'У пользователя отсувствует никнейм'
     channel = dbot.get_channel(int(config["Discord"]["channel_id"]))
     picture = discord.File(photo)
-    await channel.send(file=picture)
+    await channel.send(message, file=picture)
 
 
 @dp.message_handler(content_types=ContentType.PHOTO)
@@ -24,10 +28,28 @@ async def photo_sender(message: types.Message):
     full_path = config["Filesystem"]["path"]+filename+".jpg"
 
     await message.photo[c].download(destination_file=full_path)
-    await sender(full_path)
+    await sender(full_path, message.from_user.username)
 
     os.remove(full_path)
 
+@dp.message_handler(content_types=[ContentType.VIDEO, ContentType.ANIMATION] )
+async def photo_sender(message: types.Message):
+    if not os.path.exists(config["Filesystem"]["path"]): os.mkdir(config["Filesystem"]["path"])
+    
+    if message.video:
+        filename = message.video.file_id
+        full_path = config["Filesystem"]["path"] + filename + ".mp4"
+
+        await message.video.download(destination_file=full_path)
+    else:
+        filename = message.animation.file_id
+        full_path = config["Filesystem"]["path"] + filename + ".mp4"
+
+        await message.animation.download(destination_file=full_path)
+
+    await sender(full_path, message.from_user.username)
+
+    os.remove(full_path)
 
 @dp.message_handler()
 async def test(message):
